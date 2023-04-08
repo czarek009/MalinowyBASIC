1aa4cef5edc90e4546330f6d80a6aab53f9ff039
czarek009 <czarek.stajszczyk@protonmail.com>
2023-04-08 15:15:28 +0200

    Add printf (#9)

--- src/main.c
+++ src/main.c
@@ -1,17 +1,28 @@
 #include "uart.h"
+#include "printf.h"
 
+
+void putc(void *p, char c) {
+  if (c == '\n')
+    uart_send('\r');
+  uart_send(c);
+}
 
 void main(void){
   uart_init();
-  uart_send_string("Hello, world!\r\n");
+  init_printf(0, putc);
+
+  printf("Hello, world!\n");
+
+  int rpiv = 0;
 
   #if RPI_VERSION == 3
-  uart_send_string("RPi version: 3\r\n");
-
+    rpiv = 3;
   #elif RPI_VERSION == 4
-  uart_send_string("RPi version: 4\r\n");
-
+    rpiv = 4;
   #endif
+
+  printf("RPi version: %d\n", rpiv);
 
   while (1) {
     uart_send(uart_recv());
