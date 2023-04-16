8758bfebe7dfcfd39601ee2f783a2daa4112a8d7
Czarek Stajszczyk <czarek.stajszczyk@protonmail.com>
2023-04-16 19:53:36 +0200

    Add types and structure for aux registers

--- include/peripherials.h
+++ include/peripherials.h
@@ -1,6 +1,12 @@
 #ifndef  _PERIPHERIALS_H
 #define  _PERIPHERIALS_H
 
+#include "types.h"
+
+#ifndef RPI_VERSION
+#define RPI_VERSION 3
+#endif
+
 /* PBASE */
 #if RPI_VERSION == 3
 #define PBASE 0x3F000000
@@ -21,13 +27,62 @@
 #define GPIO_PUP_PDN_CNTRL_REG0 (PBASE + 0x002000E4) /* Pin pull-up/down for pins 15:0  */
 
 /* AUX */
-#define AUX_ENABLES     (PBASE + 0x00215004)
-#define AUX_MU_IO_REG   (PBASE + 0x00215040)
-#define AUX_MU_IER_REG  (PBASE + 0x00215044)
-#define AUX_MU_LCR_REG  (PBASE + 0x0021504C)
-#define AUX_MU_MCR_REG  (PBASE + 0x00215050)
-#define AUX_MU_LSR_REG  (PBASE + 0x00215054)
-#define AUX_MU_CNTL_REG (PBASE + 0x00215060)
-#define AUX_MU_BAUD_REG (PBASE + 0x00215068)
+struct AuxRegisters {
+  reg32 irq_status;
+  reg32 enables;
+  reg32 reserved1[14];
+  reg32 mu_io;
+  reg32 mu_ier;
+  reg32 mu_iir;
+  reg32 mu_lcr;
+  reg32 mu_mcr;
+  reg32 mu_lsr;
+  reg32 mu_msr;
+  reg32 mu_scratch;
+  reg32 mu_control;
+  reg32 mu_status;
+  reg32 mu_baud_rate;
+  reg32 reserved2[5];
+#if RPI_VERSION == 3
+  reg32 spi1_control0;
+  reg32 spi1_control1;
+  reg32 spi1_status;
+  reg32 spi1_data;
+  reg32 spi1_peek;
+  reg32 spi2_control0;
+  reg32 spi2_control1;
+  reg32 spi2_status;
+  reg32 spi2_data;
+  reg32 spi2_peek;
+#elif RPI_VERSION == 4
+  reg32 spi1_control0;
+  reg32 spi1_control1;
+  reg32 spi1_status;
+  reg32 spi1_peek;
+  reg32 spi1_data_a;
+  reg32 spi1_data_b;
+  reg32 spi1_data_c;
+  reg32 spi1_data_d;
+  reg32 spi1_ex_data_a;
+  reg32 spi1_ex_data_b;
+  reg32 spi1_ex_data_c;
+  reg32 spi1_ex_data_d;
+  reg32 spi2_control0;
+  reg32 spi2_control1;
+  reg32 spi2_status;
+  reg32 spi2_peek;
+  reg32 spi2_data_a;
+  reg32 spi2_data_b;
+  reg32 spi2_data_c;
+  reg32 spi2_data_d;
+  reg32 spi2_ex_data_a;
+  reg32 spi2_ex_data_b;
+  reg32 spi2_ex_data_c;
+  reg32 spi2_ex_data_d;
+#endif
+};
+
+// #define AUX_OFFSET 0x00215000
+#define AUX_REGS ((struct AuxRegisters *)(PBASE + 0x00215000))
 
 #endif  /*_PERIPHERIALS_H */
--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,10 @@
 #ifndef  _UTILS_H
 #define  _UTILS_H
 
-void delay(unsigned long);
-void put32(unsigned long, unsigned int);
-unsigned int get32(unsigned long);
+#include "types.h"
+
+void delay(u64 val);
+void put32(u64 addr, u32 val);
+u32 get32(u64 addr);
 
 #endif  /*_UTILS_H */
